78d98b1230543572f968698c413cd36fad7e3a1a
Gabriel Augendre <gabriel@augendre.info>
2015-10-21 08:54:26 +0200

    Reindent

--- TP2/main.c
+++ TP2/main.c
@@ -24,26 +24,25 @@
 #include "main.h"
 #include "sudoku.h"
 
-int  main(int argc,char *argv[])
-{
-  FILE *fich;
-  char *nomFich ;
-  int sudoku[9][9];
-  
-  if (argc!=2)
-    {
-      fprintf(stdout," usage: %s nomFich.txt \n",argv[0]);
-      exit(-1);
+int  main(int argc,char *argv[]) {
+    FILE *fich;
+    char *nomFich ;
+    int sudoku[9][9];
+
+    if (argc!=2) {
+        fprintf(stdout," usage: %s nomFich.txt \n",argv[0]);
+        exit(-1);
     }
-  nomFich=(char *)malloc(100*sizeof(char));
-  strcpy(nomFich,argv[1]);
-  fich=fopen(nomFich,"r");
-  if (!fich)
-    fprintf(stderr,"erreur d'ouverture du fichier\n");
-  
- lireSudoku(fich,sudoku); 
- fprintf(stdout," sudoku lu: \n");
- ecrireSudoku(stdout,sudoku);
+
+    nomFich=(char *)malloc(100*sizeof(char));
+    strcpy(nomFich,argv[1]);
+    fich=fopen(nomFich,"r");
+    if (!fich)
+        fprintf(stderr,"erreur d'ouverture du fichier\n");
+
+    lireSudoku(fich,sudoku);
+    fprintf(stdout," sudoku lu: \n");
+    ecrireSudoku(stdout,sudoku);
 
     if (sudokuValide(sudoku)) {
         printf("Et en plus il est valide !\n");
@@ -56,5 +55,5 @@ int  main(int argc,char *argv[])
         printf("La solution est valide !\n");
     }
 
-   return(0);
+    return(0);
 }
