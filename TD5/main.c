b787c54a9288557d9aba214c95293dd55bf5a6e2
Philippe Vienne <philippe@vienne.me>
2015-11-03 17:13:53 +0100

    More values in main

--- TD5/main.c
+++ TD5/main.c
@@ -3,13 +3,20 @@
 //
 
 #include <stdlib.h>
+#include <time.h>
 
 #include "tree.h"
 
 int main() {
     TREE t1, t2, tree;
     t1 = tree_create(1337, NULL, NULL);
+    int i = 0;
+    srand((unsigned int) time(NULL));
+    for (i = 0; i < 1337; i++) {
+        tree_add(t1, rand());
+    }
     t2 = tree_create(42, NULL, NULL);
     tree = tree_create(0, t1, t2);
     tree_print(tree);
+    return 0;
 }
