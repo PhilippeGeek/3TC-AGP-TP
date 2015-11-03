9215da311bfa10218bb054fac85e9a539ea0880e
Gabriel Augendre <gabriel@augendre.info>
2015-11-03 16:55:21 +0100

    Add main file

---
+++ TD5/main.c
@@ -0,0 +1,15 @@
+//
+// Created by Gabriel Augendre on 03/11/2015.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "tree.h"
+
+int main() {
+    TREE t1, t2, tree;
+    t1 = tree_create(1337, NULL, NULL);
+    t2 = tree_create(42, NULL, NULL);
+    tree = tree_create(0, t1, t2);
+}
